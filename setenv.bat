8fd0953afad72ae129335dfa2b0f8be3dd63ab24
yong.changzhang@163.com <yong.changzhang@163.com>
2019-02-13 20:52:41 +0800

    tools

---
+++ setenv.bat
@@ -0,0 +1,3 @@
+set PYTHONHOME = D:\ProgramData\Python\Python36
+set PYTHONPATH = D:\ProgramData\Python\Python36\Scripts
+set PATH=D:\ProgramData\Python\Python36;D:\ProgramData\Python\Python36\Scripts;%PATH%
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='flowpredicted',
+      version='1.0',
+	  description='OTNM2100 Flow Predicted Module',
+	  author='yczhang',
+	  author_email='yczhang@fiberhome.com',
+	  url='http://www.fiberhome.com',
+      py_modules=['bizflowpredicted','downloadrelexday','flowpredictedbase','flowpredicteddao'],)
